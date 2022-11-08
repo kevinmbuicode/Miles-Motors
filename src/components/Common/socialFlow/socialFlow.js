c1d36c783fd28d9578f17b1d112e513e960da50f
glennin-codes <glen.nyandiri@students.kyu.ac.ke>
2022-11-08 10:46:12 +0300

    feat:location

--- src/components/Common/socialFlow/socialFlow.js
+++ src/components/Common/socialFlow/socialFlow.js
@@ -1,38 +1,44 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import "./socialFlow.css"
+import "./socialFlow.css";
 import {
   faFacebook,
   faTwitter,
-  faWhatsapp,
+  faWhatsapp
 } from "@fortawesome/free-brands-svg-icons";
 
-export  default function SocialFlow (){
-     return(
-        <div>
-        <p className="social-container">
-        <a
-          href="https://www.facebook.com/learnbuildteach/"
-          className="facebook social"
-        >
-          <FontAwesomeIcon icon={faFacebook} size="2x" />
-        </a>
-        
-        <a 
-           href="https://wwww.twitter.com" 
-           className="twitter social"
-        >
-          <FontAwesomeIcon icon={faTwitter} size="2x" />
-        </a>
 
-        <a 
-           href="https://wwww.twitter.com"
-           className="whatsapp social"
-        >
-           <FontAwesomeIcon icon={faWhatsapp} size="2x"/>
-        </a>
-      </p>
-    </div>
+export default function SocialFlow() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a href="https://facebook.com/MillesMotorsLtd" >
+          <i>
+            <FontAwesomeIcon  icon= {faFacebook} className="icon"/ >
+            
+          </i>
+          <span>Facebook</span>
+          </a>
+        </li>
+        <li>
+          
+          <a href="https://twitter.com/@miles_motors">
+            <i>
+            <FontAwesomeIcon icon={faTwitter} className="icon"/>
+            </i>
+            <span>Twitter</span>
+          </a>
+        </li>
+        <li>
+          <a href="https://wa.me//+254733381707">
+            <i>
+            <FontAwesomeIcon icon={faWhatsapp} className="icon"/>
+        
+            </i>
+            <span>whatsapp</span>
+          </a>
+        </li>
+      </ul>
+    </nav>
   );
 }
-
-  
