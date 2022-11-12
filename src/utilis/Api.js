b0170cc6e05bcb1a6f9e5db2f30092e756339d32
glennin-codes <glen.nyandiri@students.kyu.ac.ke>
2022-11-12 03:16:29 +0300

    chore:contact page homepage

---
+++ src/utilis/Api.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+// *********** Send email
+export const SendEmail = async ({
+  FirstName,
+  LastName,
+  email,
+  number,
+  message,
+  setSend,
+}) => {
+  try {
+    const datas = { FirstName, LastName, email, number, message };
+    let res = await axios.post(`https://milescontact.herokuapp.com/send`, datas);
+    if (res) {
+      setSend(res.data);
+    }
+  } catch (error) {
+    alert(error.response.data.msg);
+  }
+};
