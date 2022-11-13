95670ab82a7bbb36a888d749235e76c5e08a5777
glennin-codes <glen.nyandiri@students.kyu.ac.ke>
2022-11-13 07:42:31 +0300

    merging

--- src/components/CarsSection/Courosel/ImageCourosel.js
+++ src/components/CarsSection/Courosel/ImageCourosel.js
@@ -7,48 +7,47 @@ import { useParams } from "react-router-dom";
 import { useRef } from "react";
 import "./Image.css";
 export default function ImageCarousel() {
+  const { carID } = useParams(); // get car id from url parameter
 
-const { carID } = useParams(); // get car id from url parameter
+  const [carImages, setCarImages] = useState(null);
+  // destructure car details
+  const { carImg, image2, image3, image4, image5 } = carImages ? carImages : {};
 
-const [carImages, setCarImages] = useState(null);
-// destructure car details
-const { carImg, image2, image3, image4, image5 } = carImages ? carImages : {};
+  useEffect(() => {
+    axios
+      .get(`https://milesmotors.herokuapp.com/car/${carID}`)
+      .then(({ data }) => setCarImages(data.data))
+      .catch((err) => console.log(err));
+  }, [carID]);
 
-useEffect(() => {
-  axios
-    .get(`https://milesmotors.herokuapp.com/car/${carID}`)
-    .then(({ data }) => setCarImages(data.data))
-    .catch((err) => console.log(err));
-}, [carID]);
-
-const images = [
-  {
-    id: 1,
-    src: carImg,
-    alt: "cars",
-  },
-  {
-    id: 2,
-    src: image2,
-    alt: "cars",
-  },
-  {
-    id: 3,
-    src: image3,
-    alt: "cars",
-  },
-  {
-    id: 4,
-    src: image4,
-    alt: "cars",
-  },
-  {
-    id: 5,
-    src: image5,
-    alt: "cars",
-  },
-];
-const carousel = useRef()
+  const images = [
+    {
+      id: 1,
+      src: carImg,
+      alt: "cars",
+    },
+    {
+      id: 2,
+      src: image2,
+      alt: "cars",
+    },
+    {
+      id: 3,
+      src: image3,
+      alt: "cars",
+    },
+    {
+      id: 4,
+      src: image4,
+      alt: "cars",
+    },
+    {
+      id: 5,
+      src: image5,
+      alt: "cars",
+    },
+  ];
+  const carousel = useRef();
   const settings = {
     infinite: true,
     customPaging: function (i) {
@@ -70,26 +69,27 @@ const carousel = useRef()
     slidesToShow: 1,
     arrows: true,
     slidesToScroll: 1,
-    lazyLoad: true
+    lazyLoad: true,
   };
 
-
-
-
-console.log(images);
+  console.log(images);
 
   return (
-    
     <div className="container">
-      <button onClick={() => carousel.current.slickGoTo(0)} className="button">Go to first</button>
-      <button onClick={() => carousel.current.slickGoTo(images.length - 1)}className="button">
+      <button onClick={() => carousel.current.slickGoTo(0)} className="button">
+        Go to first
+      </button>
+      <button
+        onClick={() => carousel.current.slickGoTo(images.length - 1)}
+        className="button"
+      >
         Go to last
       </button>
       <Slider {...settings} ref={carousel} className="slyder">
-      {images.map((item) => (
+        {images.map((item) => (
           <div key={item.id} className="photo--container">
             {" "}
-            <img src={item.src} alt={item.alt}className="car--photo"/>
+            <img src={item.src} alt={item.alt} className="car--photo" />
           </div>
         ))}
       </Slider>
