fb9873a09b0e5bad2e6fa29519239cd3a63a8119
glennin-codes <glen.nyandiri@students.kyu.ac.ke>
2022-11-14 14:59:45 +0300

    chore:instagram icon

--- src/components/Common/socialFlow/socialFlow.js
+++ src/components/Common/socialFlow/socialFlow.js
@@ -3,37 +3,58 @@ import "./socialFlow.css";
 import {
   faFacebook,
   faTwitter,
-  faWhatsapp
+  faWhatsapp,
+  faInstagram
 } from "@fortawesome/free-brands-svg-icons";
-
 
 export default function SocialFlow() {
   return (
     <nav>
       <ul>
         <li>
-          <a href="https://facebook.com/MillesMotorsLtd" target="_blank"    rel="noopener noreferrer">
+          <a
+            href="https://facebook.com/MillesMotorsLtd"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <i>
-              <FontAwesomeIcon  icon= {faFacebook} className="icon"/ >
-
+              <FontAwesomeIcon icon={faFacebook} className="icon" />
             </i>
-                <span>Facebook</span>
+            <span>Facebook</span>
           </a>
         </li>
         <li>
-          
-          <a href="https://twitter.com/@miles_motors" target="_blank" rel="noopener noreferrer">
+          <a
+            href="https://twitter.com/@miles_motors"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <i>
-            <FontAwesomeIcon icon={faTwitter} className="icon"/>
+              <FontAwesomeIcon icon={faTwitter} className="icon" />
             </i>
             <span>Twitter</span>
           </a>
         </li>
         <li>
-          <a href="https://wa.me//+254733381707" target="_blank" rel="noopener noreferrer">
+          <a
+            href="https://twitter.com/@miles_motors"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <i>
-            <FontAwesomeIcon icon={faWhatsapp} className="icon"/>
-        
+              <FontAwesomeIcon icon={faInstagram} className="icon" />
+            </i>
+            <span>Instagram</span>
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://wa.me//+254733381707"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i>
+              <FontAwesomeIcon icon={faWhatsapp} className="icon" />
             </i>
             <span>whatsapp</span>
           </a>
