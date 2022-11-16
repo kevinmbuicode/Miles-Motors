af21f3f0d77d11e211bc4fbbfae137946e5e0bf2
kevinmbuicode <kevinmushan@gmail.com>
2022-11-16 19:17:31 +0300

    style: about

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,34 +1,34 @@
 import { Box, Typography } from "@mui/material";
-import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 import React from "react";
-import image1 from '../images/image1.jpg'
-import image2 from '../images/image2.jpg'
-import image3 from '../images/image3.jpg'
-import image4 from '../images/image4.jpg'
-import image5 from '../images/image5.jpg'
-import image6 from '../images/image6.jpg'
-import image7 from '../images/image7.jpg'
-import image8 from '../images/image8.jpg'
-import image9 from '../images/image9.jpg'
-import image10 from '../images/image10.jpg'
-import image11 from '../images/image11.jpg'
-import image12 from '../images/image12.jpg'
-import image13 from '../images/image13.jpg'
-import image14 from '../images/image14.jpg'
-import image15 from '../images/image15.jpg'
-import image16 from '../images/image16.jpg'
-import image17 from '../images/image17.jpg'
-import image18 from '../images/image18.jpg'
-import image19 from '../images/image19.jpg'
-import image20 from '../images/image20.jpg'
-import image21 from '../images/image21.jpg'
-import image22 from '../images/image22.jpg'
-import image23 from '../images/image23.jpg'
-import image24 from '../images/image24.jpg'
-import image25 from '../images/image25.jpg'
-import image26 from '../images/image26.jpg'
-import image27 from '../images/image27.jpg'
-import image29 from '../images/image29.jpg'
+import image1 from "../images/image1.jpg";
+import image2 from "../images/image2.jpg";
+import image3 from "../images/image3.jpg";
+import image4 from "../images/image4.jpg";
+import image5 from "../images/image5.jpg";
+import image6 from "../images/image6.jpg";
+import image7 from "../images/image7.jpg";
+import image8 from "../images/image8.jpg";
+import image9 from "../images/image9.jpg";
+import image10 from "../images/image10.jpg";
+import image11 from "../images/image11.jpg";
+import image12 from "../images/image12.jpg";
+import image13 from "../images/image13.jpg";
+import image14 from "../images/image14.jpg";
+import image15 from "../images/image15.jpg";
+import image16 from "../images/image16.jpg";
+import image17 from "../images/image17.jpg";
+import image18 from "../images/image18.jpg";
+import image19 from "../images/image19.jpg";
+import image20 from "../images/image20.jpg";
+import image21 from "../images/image21.jpg";
+import image22 from "../images/image22.jpg";
+import image23 from "../images/image23.jpg";
+import image24 from "../images/image24.jpg";
+import image25 from "../images/image25.jpg";
+import image26 from "../images/image26.jpg";
+import image27 from "../images/image27.jpg";
+import image29 from "../images/image29.jpg";
 // import image29 from '../images/image29.jpg'
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
@@ -37,7 +37,7 @@ const About = () => {
   const theme = createTheme({
     typography: {
       fontFamily: ["roboto"].join(","),
-      fontWeightLight:600,
+      fontWeightLight: 600,
     },
   });
 
@@ -48,7 +48,6 @@ const About = () => {
           marginTop: 2,
           marginLeft: 5,
           marginRight: 4,
-          
         }}
       >
         <Typography
@@ -59,22 +58,33 @@ const About = () => {
           About Us
         </Typography>
         <Typography variant="p" sx={{ fontSize: "1rem" }}>
-          Miles Motors Ltd was established in 2011. We are a Brand New & Used
-          Car Dealership that is situated along Chyulu road, behind The Kenya
-          National Library in Upper-hill, Nairobi. We offer the best deal prices
-          around and also keep up to our record of being Kenya's Number 1 local
-          and international car dealership. Miles Motors Ltd was established in
-          2011. We are a Brand New & Used Car Dealership that is situated along
-          Chyulu road, behind The Kenya National Library in Upper-hill, Nairobi.
-          We offer the best deal prices around and also keep up to our record of
-          being Kenya's Number 1 local and international car dealership.
+          <Typography>Miles Motors Ltd was established in 2011.</Typography>  
+          <Typography>
+            We are a Brand New & Used Car Dealership that is situated along
+            Chyulu road, behind The Kenya National Library in Upper-hill,
+            Nairobi.
+          </Typography>
+          <Typography>
+            We offer the best deal prices around and also keep up to our record
+            of being Kenya's Number 1 local and international car dealership.
+          </Typography>{" "}
+          <Typography>
+            Car Dealership that is situated along
+            Chyulu road, behind The Kenya National Library in Upper-hill,
+            Nairobi.
+          </Typography>
+          <Typography>We offer the best deal prices around and also keep up to our record of
+          being Kenya's Number 1 local and international car dealership.</Typography>
         </Typography>
-        <Typography variant="h5" sx={{ marginTop: 1, color: "red" }}>
+        <Typography
+          variant="h5"
+          sx={{ marginTop: 1, color: "red", textAlign: "center" }}
+        >
           Services Offered
         </Typography>
         <Box>
           <ol>
-            <li>(1)  Car Sales. i.e: Brand New and Foreign Used cars.</li>
+            <li>(1) Car Sales. i.e: Brand New and Foreign Used cars.</li>
             <li>
               (2) Trade-in: For those wanting to upgrade from their current used
               units to a newer version.
@@ -82,8 +92,8 @@ const About = () => {
             <li>(3) Importation of desired cars on behalf of customers.</li>
             <li>(4) Buy locally used cars for re-sale.</li>
             <li>
-              (5) Liaise with financial institutions, i.e: Banks & Micro Finance to
-              provide car financing.
+              (5) Liaise with financial institutions, i.e: Banks & Micro Finance
+              to provide car financing.
             </li>
             <li>
               (6) Liaise with Insurance firms to provide insurance for customers
@@ -91,11 +101,11 @@ const About = () => {
             </li>
           </ol>
         </Box>
-        <Typography variant="h5" color="red">
+        <Typography variant="h5" color="red" sx={{ textAlign: "center" }}>
           Our Brands
         </Typography>
         <Typography variant="p" fontStyle="sans-serif">
-          We stock a wide variety of brands including, but not limited to;
+          We stock a wide variety of brands including, but not limited to:
           Toyota, Mercedes Benz, Lexus, Nissan, Subaru, Mazda, Isuzu, Suzuki,
           Volkswagen, Range Rover, Hyundai, Mitsubishi, Volvo, Honda, BMW, Audi,
           Jeep etc.
